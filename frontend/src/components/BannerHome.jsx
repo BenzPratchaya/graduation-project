168add30e50544ee2f29f8844aa61900a76bb51f
Pratchaya <pratchaya.tanapibulphol@gmail.com>
2024-05-23 00:26:16 +0700

    [frontend] Update banner & install fontawesome

--- frontend/src/components/BannerHome.jsx
+++ frontend/src/components/BannerHome.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./css/BannerHome.css";
-import { colors } from "@mui/material";
 
 function BannerHome(props) {
   return (
@@ -9,8 +7,7 @@ function BannerHome(props) {
       <section
         className="banner"
         style={{
-          backgroundImage:
-            'url("https://kauveryhospital.com/blog/wp-content/uploads/2023/05/Learn-How-to-Perform-First-Aid.jpg")',
+          backgroundImage: 'url("http://localhost:3000/images/banner.jpg")',
           height: "500px",
           backgroundSize: "cover",
           backgroundPosition: "center",
@@ -21,12 +18,16 @@ function BannerHome(props) {
       >
         <div className="container">
           <div className="content">
-            <h1 id="banner_Tx" style={{ textAlign: "center", fontSize: "48px"}}>
+            <h1
+              id="banner_Tx"
+              style={{ textAlign: "center", fontSize: "48px", color: "black" }}
+            >
               {props.title}
             </h1>
-            <p style={{ textAlign: "center", fontSize: "24px" }}>
-              Home / Pages {" "}
-              <span >{props.page}</span>
+            <p
+              style={{ textAlign: "center", fontSize: "24px", color: "black" }}
+            >
+              Home / Pages <span>{props.page}</span>
             </p>
           </div>
         </div>
--- frontend/src/components/Bannerpage.jsx
+++ frontend/src/components/Bannerpage.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
 function Banner(props) {
   return (
@@ -14,8 +18,34 @@ function Banner(props) {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          position: "relative",
         }}
       >
+        <div
+          style={{
+            position: "absolute",
+            top: "10px",
+            left: "10px",
+          }}
+        >
+          <Link to="/article">
+            <FontAwesomeIcon
+              icon={faCircleLeft}
+              style={{
+                color: "rgba(0, 0, 0, 0.3)",
+                padding: "10px",
+                width: "40px",
+                height: "40px",
+              }}
+              onMouseOver={(e) => {
+                e.currentTarget.style.color = "rgba(0, 0, 0, 0.7)";
+              }}
+              onMouseOut={(e) => {
+                e.currentTarget.style.color = "rgba(0, 0, 0, 0.3)";
+              }}
+            />
+          </Link>
+        </div>
         <div className="container">
           <div className="content">
             <h1
